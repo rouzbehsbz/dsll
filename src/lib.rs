e20ebf5ac02ae980b6ac2e46445596487af2a868
Rouzbeh Sabzehei <rouzbehsbz@gmail.com>
2023-11-06 12:35:36 +0330

    initialize project

---
+++ src/lib.rs
@@ -0,0 +1,111 @@
+use std::{fmt::Debug, sync::{Arc, Mutex}};
+
+type SafeNode<T> = Arc<Mutex<Option<Node<T>>>>;
+
+#[derive(Clone, Debug)]
+pub struct Node<T>
+where T: Ord + Clone + Debug + Default
+{
+    pub value: T,
+    pub next: SafeNode<T>,
+    pub prev: SafeNode<T>,
+    pub is_helper: bool
+}
+
+impl <T> Node<T>
+where T: Ord + Clone + Debug + Default
+{
+    pub fn new(value: T) -> Self {
+        Self {
+            value,
+            next: Arc::new(Mutex::new(None)),
+            prev: Arc::new(Mutex::new(None)),
+            is_helper: false
+        }
+    }
+
+    pub fn new_helper_node() -> Self {
+        Self {
+            value: T::default(),
+            next: Arc::new(Mutex::new(None)),
+            prev: Arc::new(Mutex::new(None)),
+            is_helper: true
+        }
+    }
+}
+
+pub struct DoublySortedLinkedList<T>
+where T: Ord + Clone + Debug + Default
+{
+    pub head: SafeNode<T>,
+    pub tail: SafeNode<T>,
+}
+
+impl <T> DoublySortedLinkedList<T>
+where T: Ord + Clone + Debug + Default
+{
+    pub fn new() -> Self {
+        let helper_node = Node::<T>::new_helper_node();
+
+        let head = Arc::new(Mutex::new(Some(helper_node.clone())));
+        let tail = Arc::new(Mutex::new(Some(helper_node.clone())));
+
+        head.lock().unwrap().as_mut().unwrap().next = tail.clone();
+        tail.lock().unwrap().as_mut().unwrap().prev = head.clone();
+
+        Self {
+            head,
+            tail
+        }
+    }
+
+    pub fn insert(&self, value: T) {
+        let mut current_node = self.head.clone();
+
+        loop {
+            let mut guarded_current_node = current_node.lock().unwrap();
+            let next_node = guarded_current_node.as_ref().unwrap().next.clone();
+            let mut guarded_next_node = next_node.lock().unwrap();
+
+            if guarded_next_node.as_ref().unwrap().is_helper || value < guarded_next_node.as_ref().unwrap().value {
+                let mut new_node = Node::new(value.clone());
+
+                new_node.next = next_node.clone();
+                new_node.prev = current_node.clone();
+
+                let safe_new_node = Arc::new(Mutex::new(Some(new_node)));
+
+                guarded_next_node.as_mut().unwrap().prev = safe_new_node.clone();
+                guarded_current_node.as_mut().unwrap().next = safe_new_node.clone();
+
+                break;
+            } else {
+                drop(guarded_current_node);
+
+                current_node = next_node.clone();
+            }
+        }
+    }
+
+    pub fn print(&self) {
+        let mut current_node = self.head.clone();
+
+        loop {
+            let guarded_current_node = current_node.lock().unwrap();
+
+            if guarded_current_node.is_none() {
+                break;
+            }
+
+            if !guarded_current_node.as_ref().unwrap().is_helper {
+                println!("{:?}", guarded_current_node.as_ref().unwrap().value);
+            }
+
+            let next_node = guarded_current_node.as_ref().unwrap().next.clone();
+
+            drop(guarded_current_node);
+    
+            current_node = next_node;   
+        }
+    }
+}
